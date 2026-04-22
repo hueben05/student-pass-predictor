c354a8627b550d289043955dc5148b1c345770e4
hueben05 <bennett.huey05@gmail.com>
2026-04-22 18:43:07 -0400

    Clean dataset setup
    Replaced the placeholder for loading dataset with a hardcoded student dataset and created a DataFrame.

--- main.py
+++ main.py
@@ -3,7 +3,22 @@
 # Step 1: Import libraries
 import pandas as pd
 
-# Step 2: Load dataset (will add later)
-# data = pd.read_csv("data.csv")
+# Step 2: Create dataset
 
-print("Project setup complete. Dataset will be added soon.")
+student_data = [
+    [8, 100, 100, 90, 1],
+    [3, 50, 62, 70, 0],
+    [5, 85, 86, 87, 1]
+]
+
+data = pd.DataFrame(student_data, columns=[
+    "study_hours",
+    "attendance_percent",
+    "homework_completion_percent",
+    "quiz_average",
+    "passed"
+])
+
+print(data)
+
+print("Project setup complete.")
